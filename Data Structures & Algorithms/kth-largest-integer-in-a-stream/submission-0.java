5700b77060b57cd5333f5d391cbae275289ddf21
Pedro Obi <75422167+pedroski121@users.noreply.github.com>
2026-05-03 23:22:54 +0100

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,21 @@
+class KthLargest {
+
+    List<Integer> arr;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        arr = new ArrayList<>();
+        this.k = k;
+        for(int i = 0; i< nums.length; i++){
+            arr.add(nums[i]);
+        }
+    }
+    
+    public int add(int val) {
+        arr.add(val);
+        Collections.sort(arr);
+
+        return arr.get(arr.size() - k);
+
+    }
+}
